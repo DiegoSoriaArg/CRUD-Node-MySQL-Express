58297565b08ccf32d3d15b9b84f8cd2216ba6021
DiegoSoriaArg <diegosoriaarg@gmail.com>
2023-05-23 15:18:04 -0300

    Subiendo el proyecto

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+
+const app = express();
+
+app.get("/empleados", (req, res) => {
+    res.send("Obteniendo empleados");
+});
+
+app.post("/empleados", (req, res) => {
+    res.send("Creando empleados");
+});
+
+app.put("/empleados", (req, res) => {
+    res.send("Actualizando empleados");
+});
+
+app.delete("/empleados", (req, res) => {
+    res.send("Eliminando empleados");
+});
+
+app.listen(3000);
+console.log("Server running on port 3000");
