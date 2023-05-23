d08716d269d99d89bb9ae517e8f93c63cc8946aa
DiegoSoriaArg <diegosoriaarg@gmail.com>
2023-05-23 20:55:11 -0300

    Not Found

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ const app = express();
 app.use(express.json());
 app.use(indexRoutes);
 app.use("/api",router);
+app.use((req, res, next) => {
+    res.status(404).json({
+        message: " EndPoint Not Found"
+    });
+});
 
 app.listen(3000);
 console.log("Server running on port 3000");
